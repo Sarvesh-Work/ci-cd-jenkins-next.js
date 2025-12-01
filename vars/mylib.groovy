6272d967dcb963f89a911f22d571411f7b8e8f7d
Sarvesh Munde <sarveshmunde10@gmail.com>
2025-12-01 17:41:08 +0530

    add shared lib functions for building and deploying Docker images

--- vars/mylib.groovy
+++ vars/mylib.groovy
@@ -3,3 +3,36 @@ def codePulling(gitUrl,branch){
     git url: "${gitUrl}", branch: "${branch}"
     echo 'Done pulling code'
 }
+
+
+def buildImage(image,tag){
+    echo 'Build started'
+    sh 'docker build -t $image:$tag .'
+    echo 'Done building'
+}
+
+
+def pushToDockerHub(image,tag){
+    withCredentials([
+    usernamePassword(
+    credentialsId: 'dockerHub',
+    usernameVariable: 'DOCKER_USER',
+    passwordVariable: 'DOCKER_PASS'
+    )
+    ]) {
+    echo 'Pushing image'
+
+    sh "docker login -u $DOCKER_USER -p $DOCKER_PASS"
+    sh "docker tag  ${image}:${tag} $DOCKER_USER/${image}:${tag}"
+    sh "docker push $DOCKER_USER/${image}:${tag}"
+
+    echo 'Done Pushing'
+    }
+}
+
+ //  ONLY FOR TESTING IN PRODUCTION ENV WE USE SEPARATE  EC2 FOR TESTING SND DEPLOYMENT 
+def deployApp(dockerHubRepo,image){
+    echo 'Started using image from DockerHub'
+    sh 'docker run -d -p 3000:3000 $dockerHubRepo/$image'
+    echo 'Done building'
+}
